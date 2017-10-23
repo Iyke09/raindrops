6977d5e6c9aef123e4b59d099918be81758bacad
Emma Paris <iykay33@gmail.com>
2017-10-23 21:09:29 +0100

    solution to raindrop algo

--- src/app.js
+++ src/app.js
@@ -1,46 +1,51 @@
 module.exports = {
-rainDrop: (num) => {
-  let arr = [];
-  let res = [];
-  let rain = [];
-  if(num === undefined){
-    return 'please enter a value'
-  }
-  if(typeof(num) !== 'number'){
-    return 'please enter a number'
-  }
-  if(num === 1){
-    return 1
-  }
-  if(num <= 0){
-    return 'please enter a value > 0'
-  }
-  let number = num;
-  for(let i = 2;i <= number;i++){
-    while(number % i === 0){
-      arr.push(i);
-      number = number / i
+  rainDrop: (num) => {
+    let arr = [];
+    let result = [];
+    let rainDropArr = [];
+    //if value entered is undefined
+    if(num === undefined){
+      return 'please enter a value'
     }
-  }
-  for(let x of arr){
-    if(x === 3 && res.indexOf(x) === -1){
-      res.push(x)
-      rain.push('Pling')
+    //if value entered is not a number
+    if(typeof(num) !== 'number'){
+      return 'please enter a number'
     }
-    if(x === 5 && res.indexOf(x) === -1){
-      res.push(x)
-      rain.push('Plang')
+    //if value entered is equal to 1
+    if(num === 1){
+      return 1
     }
-    if(x === 7 && res.indexOf(x) === -1){
-      res.push(x)
-      rain.push('Plong')
+    //if value entered is less than 0 or negative
+    if(num <= 0){
+      return 'please enter a value > 0'
     }
-    if(arr.indexOf(3) === -1 && arr.indexOf(5) === -1 && arr.indexOf(7) === -1){
-      return num
+    let number = num;
+    for(let i = 2;i <= number;i++){
+      while(number % i === 0){//check if there is a remainder
+        arr.push(i);
+        number = number / i
+      }
     }
+    for(let x of arr){
+      //check if x = 3 and is in result array
+      if(x === 3 && result.indexOf(x) === -1){
+        result.push(x)
+        rainDropArr.push('Pling')
+      }
+      if(x === 5 && result.indexOf(x) === -1){
+        result.push(x)
+        rainDropArr.push('Plang')
+      }
+      if(x === 7 && result.indexOf(x) === -1){
+        result.push(x)
+        rainDropArr.push('Plong')
+      }
+      //if 3 or 5 or 7 is in arr
+      if(arr.indexOf(3) === -1 && arr.indexOf(5) === -1 && arr.indexOf(7) === -1){
+        return num
+      }
 
+    }
+    return rainDropArr.join('')
   }
-  return rain.join('')
-}
-
 }
