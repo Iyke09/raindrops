61780b66a907661a77505485e9ab0adcfb13b97c
Andela Developer <andeladeveloper@Andelas-MacBook-Pro-17.local>
2018-05-04 22:12:16 +0100

    add new test for sqareFunc function

--- src/app.js
+++ src/app.js
@@ -1,52 +1,8 @@
 module.exports = {
-  rainDrop: (num) => {
-    let arr = [];
-    let result = [];
-    let rainDropArr = [];
-    //if value entered is undefined
-    if(num === undefined){
-      return 'please enter a value'
-    }
-    //if value entered is not a number
-    if(typeof(num) !== 'number'){
-      return 'please enter a number'
-    }
-    //if value entered is equal to 1
-    if(num === 1){
-      return 1
-    }
-    //if value entered is less than 0 or negative
-    if(num <= 0){
-      return 'please enter a value > 0'
-    }
-    let number = num;
-    for(let i = 2;i <= number;i++){
-      while(number % i === 0){//check if there is a remainder
-        arr.push(i);
-        number = number / i
-      }
-    }
-    for(let x of arr){
-      //check if x = 3 and is in result array
-      if(x === 3 && result.indexOf(x) === -1){
-        result.push(x)
-        rainDropArr.push('Pling')
-      }
-      //check if x = 5 and is in result array
-      if(x === 5 && result.indexOf(x) === -1){
-        result.push(x)
-        rainDropArr.push('Plang')
-      }
-      if(x === 7 && result.indexOf(x) === -1){
-        result.push(x)
-        rainDropArr.push('Plong')
-      }
-      //if 3 or 5 or 7 is in arr
-      if(arr.indexOf(3) === -1 && arr.indexOf(5) === -1 && arr.indexOf(7) === -1){
-        return num
-      }
-
-    }
-    return rainDropArr.join('')
+  squareFunc: (num) => {
+    const arr = num.toString().split('')
+    const newArr = arr.map((x) => x ** 2);
+    
+    return newArr.join('');
   }
 }
--- test/test.js
+++ test/test.js
@@ -1,40 +1,19 @@
 const assert = require('chai').assert;
 const myApp = require ('../src/app.js');
 
-describe('Raindrop-Speak', () => {
+describe('SquareFunc', () => {
   describe('handle valid input', () => {
-    it('should return Plong when the input is 28', () => {
-      assert.equal(myApp.rainDrop(28),'Plong');
+    it('should return 811181 when the input is 9119', () => {
+      assert.equal(myApp.squareFunc(9119), 811181);
     });
-    it('should return PlingPlang when the input is 1755', () => {
-      assert.equal(myApp.rainDrop(1755),'PlingPlang');
+    it('should return 916 when the input is 34', () => {
+      assert.equal(myApp.squareFunc(34), 916);
     });
-    it('should return 34 when the input is 34', () => {
-      assert.equal(myApp.rainDrop(34),34);
-    });
-    it('should return PlingPlang when the input is 15', () => {
-      assert.equal(myApp.rainDrop(15),'PlingPlang');
-    });
-    it('should return 1 when the input is 1', () => {
-      assert.equal(myApp.rainDrop(1),1);
+    it('should return 125 when the input is 15', () => {
+      assert.equal(myApp.squareFunc(15), 125);
     });
   });
 });
 
-describe('Raindrop-Speak', () => {
-  describe('handle invalid input', () => {
-    it('should return please enter value for empty values', () => {
-      assert.equal(myApp.rainDrop(),'please enter a value');
-    });
-    it('should return invalid input when the input is of type string', () => {
-      assert.equal(myApp.rainDrop('hello'),"please enter a number");
-    });
-    it('should return invalid input when the input is null', () => {
-      assert.equal(myApp.rainDrop(null),"please enter a number");
-    });
-    it('should return invalid input when the input is of type is less than 0', () => {
-      assert.equal(myApp.rainDrop(-890),"please enter a value > 0");
-    });
-  });
-});
+
 
